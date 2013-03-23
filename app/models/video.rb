f75daeead859e5e6d9f27140da969d641eabab2d
Ryan MacInnes <ryan.macinnes@gmail.com>
2013-03-22 20:17:56 -0400

    Photos are way cooler now.

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,34 +1,34 @@
 class Video < ActiveRecord::Base
-  
+
   if RAILS_ENV == "development"
-    has_attached_file :thumb, :styles => { :thumb => "196x110#", :poster => "840x473#"},
-                              :storage => :s3, 
-                              :s3_credentials => "#{RAILS_ROOT}/config/s3.yml", 
+    has_attached_file :thumb, :styles => { :thumb => "290x163#", :poster => "840x473#"},
+                              :storage => :s3,
+                              :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
                               :path => ':id/:style'#,
                               # :s3_host_alias => 'd2eg9jtzojfpjk.cloudfront.net',
                               #:url => ':s3_alias_url'
-                              
-    has_attached_file :video, :storage => :s3, 
-                              :s3_credentials => "#{RAILS_ROOT}/config/s3-video.yml", 
+
+    has_attached_file :video, :storage => :s3,
+                              :s3_credentials => "#{RAILS_ROOT}/config/s3-video.yml",
                               :path => ':id',
                               :s3_host_alias => 'dcjd67wbyjfmb.cloudfront.net',
                               :url => ':s3_alias_url'
-                              
-    has_attached_file :ogg, :storage => :s3, 
-                              :s3_credentials => "#{RAILS_ROOT}/config/s3-ogg.yml", 
+
+    has_attached_file :ogg, :storage => :s3,
+                              :s3_credentials => "#{RAILS_ROOT}/config/s3-ogg.yml",
                               :path => ':id',
                               :s3_host_alias => 'd2zv9v2awdzw9v.cloudfront.net',
                               :url => ':s3_alias_url'
-                              
+
   elsif RAILS_ENV == "production"
-    has_attached_file :thumb, :styles => { :thumb => "196x110#", :poster => "840x473#"},
+    has_attached_file :thumb, :styles => { :thumb => "290x163#", :poster => "840x473#"},
                               :storage => :s3, 
-                              :s3_credentials => "#{RAILS_ROOT}/config/s3.yml", 
+                              :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
                               :path => ':id/:style',
                               :s3_host_alias => 'd116frzuh3c2sg.cloudfront.net',
                               :url => ':s3_alias_url'
-                              
-    has_attached_file :video, :storage => :s3, 
+
+    has_attached_file :video, :storage => :s3,
                               :s3_credentials => "#{RAILS_ROOT}/config/s3-video.yml", 
                               :path => ':id',
                               :s3_host_alias => 'd36ymaapgpqd1v.cloudfront.net',
