d1d9f3d632f7f3054c175c35b6d475b0386ce487
Ryan MacInnes <ryan.macinnes@gmail.com>
2013-06-30 22:26:25 -0400

    Dynamically sizing images with javascript.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,65 +1,80 @@
+function resizeThumbnails() {
+  var images = $('#videos img');
+  var lists = $('#videos li')
+  var width = images.width();
+  var height = width / 1.78 + 'px';
+  images.css('height', height);
+  lists.find('p').css('line-height', height);
+  lists.css('height', height)
+}
+
 $(document).ready(function(){
-	
-	$("#work-nav a.work").mouseenter(function () {
-		if ($("div#videos").is(":hidden")) {
-			$("#work-nav a.work").html("Sean Meehan");
-		}
-		return false
-	});
-	
-	$("#work-nav a.work.work").mouseleave(function () {
-		if ($("div#videos").is(":hidden")) {
-			$("#work-nav a.work").html("Work");
-		}
-		return false
-	});
-	
-	$("#contact-nav a.contact").mouseenter(function () {
-		if ($("div#contact").is(":hidden")) {
-			$("#contact-nav a.contact").html("Sam McGarry");
-		}
-		return false
-	});
-	
-	$("#contact-nav a.contact").mouseleave(function () {
-		if ($("div#contact").is(":hidden")) {
-			$("#contact-nav a.contact").html("Contact");
-		}
-		return false
-	});
-	
-	$("#work-nav a.work").click(function () {
-		if ($("div#videos").is(":hidden")) {
-			if ($("div#contact").is(":visible")) {
-				$("div#contact").slideUp();
-			}
-			$("div#videos").slideDown();
-			$("#work-nav a.work").html("Sean Meehan");
-			$("#work-nav a.work").addClass("border-bottom");
-			
-		} else {
-			$("div#videos").slideUp();
-			$("#work-nav a.work").html("Work");
-			$("#work-nav a.work").removeClass("border-bottom");
-		}
-		return false
-	});
-	
-	$("#contact-nav a.contact").click(function () {
-		if ($("div#contact").is(":hidden")) {
-			if ($("div#videos").is(":visible")) {
-				$("div#videos").slideUp();
-				$("#work-nav a.work").html("Work");
-				$("#work-nav a.work").removeClass("border-bottom");
-			}	
-			$("div#contact").slideDown();
-			$("#contact-nav a.contact").html("Sam McGarry");
-		} else {
-			$("div#contact").slideUp();
-			$("#contact-nav a.contact").html("Contact");
-		}
-		return false
-	});
+
+        $("#work-nav a.work").mouseenter(function () {
+                if ($("div#videos").is(":hidden")) {
+                        $("#work-nav a.work").html("Sean Meehan");
+                }
+                return false
+        });
+
+        $("#work-nav a.work.work").mouseleave(function () {
+                if ($("div#videos").is(":hidden")) {
+                        $("#work-nav a.work").html("Work");
+                }
+                return false
+        });
+
+        $("#contact-nav a.contact").mouseenter(function () {
+                if ($("div#contact").is(":hidden")) {
+                        $("#contact-nav a.contact").html("Sam McGarry");
+                }
+                return false
+        });
+
+        $("#contact-nav a.contact").mouseleave(function () {
+                if ($("div#contact").is(":hidden")) {
+                        $("#contact-nav a.contact").html("Contact");
+                }
+                return false
+        });
+
+        $("#work-nav a.work").click(function () {
+                if ($("div#videos").is(":hidden")) {
+                        if ($("div#contact").is(":visible")) {
+                                $("div#contact").slideUp();
+                        }
+                        $("div#videos").slideDown();
+                        $("#work-nav a.work").html("Sean Meehan");
+                        $("#work-nav a.work").addClass("border-bottom");
+                        resizeThumbnails()
+
+
+                } else {
+                        $("div#videos").slideUp();
+                        $("#work-nav a.work").html("Work");
+                        $("#work-nav a.work").removeClass("border-bottom");
+                }
+                return false
+        });
+
+        $("#contact-nav a.contact").click(function () {
+                if ($("div#contact").is(":hidden")) {
+                        if ($("div#videos").is(":visible")) {
+                                $("div#videos").slideUp();
+                                $("#work-nav a.work").html("Work");
+                                $("#work-nav a.work").removeClass("border-bottom");
+                        }
+                        $("div#contact").slideDown();
+                        $("#contact-nav a.contact").html("Sam McGarry");
+                } else {
+                        $("div#contact").slideUp();
+                        $("#contact-nav a.contact").html("Contact");
+                }
+                return false
+        });
 
 });
 
+$(window).resize(function() {
+  resizeThumbnails()
+});
